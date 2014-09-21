9bb8aca073d6cbcf67cba0f736bbbe4121b7d480
mAarnos <mikko.aarnos@kolumbus.fi>
2014-09-21 20:16:09 +0300

    Added clamp-function.

---
+++ src/clamp.hpp
@@ -0,0 +1,12 @@
+#ifndef CLAMP_HPP_
+#define CLAMP_HPP_
+
+#include <algorithm>
+
+template<class T>
+T clamp(T value, T lowerBound, T upperBound)
+{
+    return std::max(lowerBound, std::min(value, upperBound));
+}
+
+#endif
