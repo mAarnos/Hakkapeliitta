1e2081d8bc6df5a4ab8efdf554cb27700b80537c
mAarnos <mikko.aarnos@kolumbus.fi>
2014-10-17 10:40:01 +0300

    Reverse futility now in PV-nodes.

--- src/search.cpp
+++ src/search.cpp
@@ -468,7 +468,7 @@ int alphabetaPVS(Position & pos, int ply, int depth, int alpha, int beta, int al
     staticEval = (inCheck ? 0 : eval(pos));
 
     // Reverse futility pruning.
-    if (!pvNode && !inCheck && pos.getPhase() != totalPhase 
+    if (!inCheck && pos.getPhase() != totalPhase 
         && depth <= staticNullMoveDepth && staticEval - staticNullMoveMargin[depth] >= beta)
         return staticEval - staticNullMoveMargin[depth];
 
