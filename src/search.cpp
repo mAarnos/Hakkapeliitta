9739e25009d85b6e372cf6c29190ad94d88792e3
mAarnos <mikko.aarnos@kolumbus.fi>
2015-01-01 11:11:15 +0200

    Fixed LMP condition.

--- src/search.cpp
+++ src/search.cpp
@@ -767,7 +767,7 @@ int Search::search(Position& pos, int depth, int ply, int alpha, int beta, int a
         auto nonCriticalMove = !extension && move.getScore() >= 0 && move.getScore() < killerMoveScore[4];
 
         // Futility pruning and late move pruning / move count based pruning.
-        if (nonCriticalMove && (futileNode || (lmpNode && movesSearched >= lmpMoveCount[depth])))
+        if (nonCriticalMove && (futileNode || (lmpNode && i >= lmpMoveCount[depth])))
         {
             pos.unmakeMove(move, history);
             ++prunedMoves;
