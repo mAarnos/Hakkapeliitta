63af414ae619ce6e434f032c02796dbbc379c1eb
mAarnos <mikko.aarnos@kolumbus.fi>
2015-01-22 17:59:11 +0200

    Added most of TT back.

---
+++ src/tt.cpp
@@ -0,0 +1,158 @@
+#include "tt.hpp"
+#include <cassert>
+#include <cmath>
+#include <unordered_set>
+#include "utils/clamp.hpp"
+// #include "search.hpp"
+
+TranspositionTable::TranspositionTable():
+table(nullptr), tableSize(0)
+{
+    setSize(32); 
+}
+
+TranspositionTable::~TranspositionTable()
+{
+    delete table;
+}
+
+void TranspositionTable::setSize(size_t sizeInMegaBytes)
+{
+    // Clear the tt completely to avoid any funny business.
+    delete table;
+
+    // If size is not a power of two make it the biggest power of two smaller than size.
+    if (sizeInMegaBytes & (sizeInMegaBytes - 1))
+    {
+        sizeInMegaBytes = static_cast<int>(std::pow(2, std::floor(std::log2(sizeInMegaBytes))));
+    }
+
+    tableSize = ((sizeInMegaBytes * 1024 * 1024) / sizeof(Cluster));
+    table = static_cast<Cluster*>(malloc(tableSize * sizeof(Cluster)));
+    clear();
+}
+
+void TranspositionTable::clear()
+{
+    memset(table, 0, sizeof(Cluster) * tableSize);
+    generation = 1;
+}
+
+void TranspositionTable::prefetch(const HashKey hk)
+{
+    auto address = reinterpret_cast<char*>(&table[hk & (tableSize - 1)]);
+#if defined (_MSC_VER) || defined(__INTEL_COMPILER)
+    _mm_prefetch(address, _MM_HINT_T0);
+#else
+    __builtin_prefetch(address);
+#endif
+}
+
+void TranspositionTable::save(const HashKey hk, const int ply, const Move& move, const int score, const int depth, const int flags)
+{
+    assert(ply >= 0 && ply < 128);
+    assert(depth >= 0 && depth <= 255);
+    // assert(score < infinity && score > -infinity);
+    assert(flags >= 0 && flags <= 255);
+
+    auto best = move.getMove();
+    auto hashEntry = table[hk & (tableSize - 1)].entries;
+    auto replace = hashEntry;
+
+    // We only store pure mate scores so that we can use them in other parts of the search tree too.
+    /*
+    if (isMateScore(score))
+    {
+        score += (score > 0 ? ply : -ply);
+    }
+    */
+
+    for (auto i = 0; i < 4; ++i, ++hashEntry)
+    {
+        if ((hashEntry->getHash() ^ hashEntry->getData()) == hk)
+        {
+            replace = hashEntry;
+            if (!best)
+            {
+                best = hashEntry->getBestMove();
+            }
+            break;
+        }
+
+        // Here we check if we have found an entry which is worse than the current worse entry.
+        // If the entry is from a earlier search or has a smaller depth it is worse and is made the new worst entry.
+        auto c1 = (replace->getGeneration() > hashEntry->getGeneration());
+        auto c2 = (replace->getDepth() > hashEntry->getDepth());
+
+        if (c1 || c2)
+        {
+            replace = hashEntry;
+        }
+    }
+
+    replace->setData((static_cast<uint64_t>(best) | 
+                      static_cast<uint64_t>(generation) << 16 | 
+                      static_cast<uint64_t>(score & 0xffff) << 32 | 
+                      static_cast<uint64_t>(depth & 0xff) << 48) | 
+                      static_cast<uint64_t>(flags) << 56);
+    replace->setHash(hk ^ replace->getData());
+
+    assert(replace->getBestMove() == best);
+    assert(replace->getGeneration() == generation);
+    assert(replace->getScore() == score);
+    assert(replace->getDepth() == depth);
+    assert(replace->getFlags() == flags);
+}
+
+const TranspositionTableEntry* TranspositionTable::probe(const HashKey hk) const
+{
+    const auto* hashEntry = &table[hk & (tableSize - 1)].entries[0];
+
+    for (auto i = 0; i < 4; ++i, ++hashEntry)
+    {
+        if ((hashEntry->getHash() ^ hashEntry->getData()) == hk)
+        {
+            return hashEntry;
+        }
+    }
+
+    return nullptr;
+}
+
+std::vector<Move> TranspositionTable::extractPv(Position root) const
+{
+    std::vector<Move> pv;
+    std::unordered_set<HashKey> previousHashes;
+    Move m;
+
+    for (auto ply = 0; ply < 128; ++ply)
+    {
+        auto entry = probe(root.getHashKey());
+
+        // No entry found -> end of PV
+        if (!entry) 
+            break;
+
+        // No move found in the entry -> cannot add to the PV -> end of PV
+        if (!entry->getBestMove()) 
+            break;
+
+        // Repetition draw -> end of PV
+        if (previousHashes.count(root.getHashKey()) > 0) 
+            break;
+
+        // No exact score hash entry to use -> end of PV 
+        // If we are very near the root we accept all flags as we absolutely need the move to be played and a ponder move is very important as well.
+        if (entry->getFlags() != ExactScore && ply >= 2) 
+            break;
+
+        m.setMove(entry->getBestMove());
+        pv.push_back(m);
+        previousHashes.insert(root.getHashKey());
+        root.makeMove(m);
+    }
+
+    return pv;
+}
+
+
---
+++ src/tt.hpp
@@ -0,0 +1,70 @@
+#ifndef TT_HPP_
+#define TT_HPP_
+
+#include <cstdint>
+#include <array>
+#include <vector>
+#include "move.hpp"
+#include "zobrist.hpp"
+#include "position.hpp"
+
+// Flags for different types of TT entries.
+// ExactScore means that the score is exactly what is in the entry.
+// UpperBoundScore means that the score is at most the score in the entry.
+// LowerBoundScore means the the score is at least the score in the entry.
+enum TTFlags
+{
+    ExactScore = 1, UpperBoundScore = 2, LowerBoundScore = 4
+};
+
+// A single entry in the transposition table. 
+class TranspositionTableEntry
+{
+public:
+    TranspositionTableEntry() { hash = 0; data = 0; }
+
+    void setHash(const uint64_t newHash) { hash = newHash; }
+    void setData(const uint64_t newData) { data = newData; }
+    uint64_t getHash() const { return hash; }
+    uint64_t getData() const { return data; }
+    uint16_t getBestMove() const { return static_cast<uint16_t>(data); }
+    uint16_t getGeneration() const { return static_cast<uint16_t>(data >> 16); }
+    int16_t getScore() const { return static_cast<int16_t>(data >> 32); }
+    uint8_t getDepth() const { return static_cast<uint8_t>(data >> 48); }
+    uint8_t getFlags() const { return data >> 56; };
+private:
+    uint64_t hash;
+    uint64_t data;
+};
+
+class TranspositionTable
+{
+public:
+    TranspositionTable();
+    ~TranspositionTable();
+
+    void save(HashKey hk, int ply, const Move& move, int score, int depth, int flags);
+    const TranspositionTableEntry* probe(HashKey hk) const;
+    void prefetch(HashKey hk);
+
+	void setSize(size_t sizeInMegaBytes);
+    void clear();
+    void startNewSearch() { ++generation; }
+    std::vector<Move> extractPv(Position root) const;
+private:
+    // A bucket containing four hash entries.
+    // Doing this has some desirable properties when deciding what entries to overwrite.
+    // We have four entries because the common cache line size nowadays is 64 bytes.
+    // uint64_t hash * 4 + uint64_t data * 4 = 8 * uint64_t = 64 bytes.
+    // Basically this means that a single cluster fits perfectly into the cacheline.
+    struct Cluster
+    {
+        TranspositionTableEntry entries[4];
+    };
+
+    Cluster* table;
+    size_t tableSize;
+    uint32_t generation;
+};
+
+#endif
