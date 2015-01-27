161bfea17f89c23ce66e99adfa9f718db6252d96
mAarnos <mikko.aarnos@kolumbus.fi>
2015-01-27 22:59:47 +0200

    Licenses at the top of every source file.

--- src/eval.hpp
+++ src/eval.hpp
@@ -1,3 +1,20 @@
+/*
+    Hakkapeliitta - A UCI chess engine. Copyright (C) 2013-2015 Mikko Aarnos.
+
+    Hakkapeliitta is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Hakkapeliitta is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hakkapeliitta. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef EVAL_HPP_
 #define EVAL_HPP_
 
